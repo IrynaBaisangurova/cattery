2e525acc66b26efd4b0d757d1075b034ef27dd92
IrynaBaisangurova <92602692+IrynaBaisangurova@users.noreply.github.com>
2022-01-13 01:01:56 +0200

    Add files via upload

---
+++ script.js/modal.js
@@ -0,0 +1,48 @@
+
+$(window).scroll(function(){
+    if($(this).scrollTop()>500){
+     $('#up').fadeIn(); 
+    }else{
+      $('#up').fadeOut();
+    }  
+  })
+  $('#up').on('click', function(){
+      $('html, body').animate({scrollTop: 0},400);
+  })
+
+  $('#button').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#contacts').offset().top},500);
+})
+
+  $('#home').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#main').offset().top},500);
+})
+  $('#about').on('click', function(e){
+      e.preventDefault();
+      $('html, body').animate({scrollTop: $('#our').offset().top},500);
+  })
+  $('#males').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#cat').offset().top},500);
+})  
+$('#females').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#cats').offset().top},500);
+})
+
+$('#kittens').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#new').offset().top},500);
+  })
+$('#gallery').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#photo').offset().top},500);
+  })
+
+  $('#contacts').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#phone').offset().top},500);
+  })
+
---
+++ script.js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow:2,
+        autoplay: true,
+        speed: 1000,
+    });
+});
+
